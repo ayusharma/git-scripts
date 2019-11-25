44a4d405afb3853802e3c127e988a53e9d6d1271
Ayush Sharma <ayush.aceit@gmail.com>
2019-11-25 17:00:00 +0100

    Update installation instructions

---
+++ README.md
@@ -0,0 +1,18 @@
+### Git scripts
+
+This repository contains useful git automation scripts for custom use.
+
+
+## 1. `git reintegrate`
+
+```sh
+    git reintegrate <target branch you want to update> <source branch for new changes>
+```
+
+Note: This uses **master-nsi** as an optional argument.
+
+so, following command will update target branch with **master-nsi**
+
+```
+    git reintegrate <target branch you want to update>
+```
--- install.sh
+++ install.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
+installationDir="${HOME}/.git-scripts"
+
+if [ -d "$installationDir" ]; then rm -Rf $installationDir; fi
 
 git clone https://github.com/ayusharma/git-scripts "${HOME}/.git-scripts"
 
-echo "Please add following line to your bash_profile ~/.bash_profile, ~/.zshrc, ~/.profile, or ~/.bashrc"
-echo ""
-echo "export PATH=\$PATH:$HOME./git-scripts"
 echo ""
 echo ""
-echo "Restart the terminal to activate"
-
+echo "Please add following line to your bash_profile ~/.bash_profile, ~/.zshrc, ~/.profile, or ~/.bashrc:"
+echo ""
+echo "export PATH=\$PATH:$HOME/.git-scripts"
+echo ""
+echo ""
+echo "Please restart the terminal to activate"
+echo ""
+echo ""
