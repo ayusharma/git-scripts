c9e0d3acb8d897b8ed27fdd14587be6c36439590
Ayush Sharma <ayush.aceit@gmail.com>
2019-11-25 14:42:06 +0100

    Fixes installation git repo

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-git clone htstps://github.com/ayusharma/git-scripts "${HOME}/.git-scripts"
+git clone https://github.com/ayusharma/git-scripts "${HOME}/.git-scripts"
 
 echo "Please add following line to your bash_profile ~/.bash_profile, ~/.zshrc, ~/.profile, or ~/.bashrc"
 echo ""
